bf8642f17ec4ad80564103327670d774a203359e
daniel-zavacky <daniel.zavacky@goodrequest.com>
2021-04-29 22:53:21 +0200

    added shop example

--- examples/bootstrap.sh
+++ examples/bootstrap.sh
@@ -20,7 +20,7 @@ then
   exit 1
 fi
 
-curl -X DELETE http://localhost:9200/$@
+curl -X DELETE http://localhost:9201/$@
 
 if [ -f "examples/$@/schema.py" ]; then
   python examples/$@/schema.py --config "$(pwd)/examples/$@/schema.json"
